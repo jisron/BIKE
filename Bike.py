38cb58cbb411374225322b6ddb0dc6fcbdfdff08
JisronMalik <37628307+jisron@users.noreply.github.com>
2023-11-22 15:10:19 +0700

    Update Bike.py

--- Bike.py
+++ Bike.py
@@ -44,7 +44,7 @@ df.head()
 ### Assessing Data
 """
 
-st.dataframe(df.info())
+st.df.info()
 
 """selanjutnya kita mencek info yang terdapat dalam data untuk melihat type data dan total data yang ada
 

b838d0bf6efc09478ecfa8101a3ca1ddd2d4b858
JisronMalik <37628307+jisron@users.noreply.github.com>
2023-11-23 08:42:55 +0700

    Update Bike.py

--- Bike.py
+++ Bike.py
@@ -110,8 +110,8 @@ st.pyplot(plt)
 
 """disini kita melakukan visualisasi data dengan menggabungkan diagram garis dan batang agar data kita dapat mudah dicerna"""
 
-sns.heatmap(df4, annot=True)
-st.pyplot(sns)
+heat = sns.heatmap(df4, annot=True)
+st.pyplot(heat.get_figure())
 
 """disini kita menggunakan heatmap untuk melihat nilai-nilai terbaik dalam data yang kita punya
 
